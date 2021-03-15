93d451f8cbed8ae8aac38a0dccc982b6fd610ed4
Divyanshi Rastogi <divyanshi.rastogi@hashouttech.com>
2021-03-15 23:33:56 +0530

    Duplicate-encoder

---
+++ duplicate-encoder.java
@@ -0,0 +1,28 @@
+// DuplicateEncoder
+// Problem: Duplicate Encoder (http://www.codewars.com/kata/duplicate-encoder)
+//The goal of this exercise is to convert a string to a new string where each character in the new string is "(" if that character appears only once in the original string, or ")" if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DuplicateEncoder {
+	static String encode(String word){
+  Map <Character,Integer> map=new HashMap<>();
+    word=word.toLowerCase();
+    for(int i=0;i<word.length();i++){
+      if(map.containsKey(word.charAt(i))){
+        int value=map.get(word.charAt(i));
+        map.remove(word.charAt(i));
+        map.put(word.charAt(i),++value);
+}
+      else{
+         map.put(word.charAt(i),1);
+}
+ }
+      String output="";
+      for(int i=0;i<word.length();i++){
+        output+=(map.get(word.charAt(i))>1)?")":"(";
+}
+      return output;
+  }
+}
